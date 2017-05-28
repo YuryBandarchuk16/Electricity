ff80f797b51039f96a9e49ff087f1d287a2c1313
YuryBandarchuk16 <yury.bandarchuk@gmail.com>
2017-05-28 17:50:37 +0300

    Main for testing updated

--- src/ru/ifmo/ctddev/bandarchuk/expression/Main.java
+++ src/ru/ifmo/ctddev/bandarchuk/expression/Main.java
@@ -3,7 +3,8 @@ package ru.ifmo.ctddev.bandarchuk.expression;
 public class Main {
 
     public static void main(String[] args) throws IncorrectExpressionException, OverflowException, DivisionByZeroException {
-        double val = new ExpressionParser().parse("sin(x + y)").evaluate(Math.PI * 0.5, 0.0, 0.0);
+        Operand parsed = new ExpressionParser().parse("sin(x)");
+        double val = parsed.diff("x").evaluate(0, 0, 0);
         System.out.println(val);
     }
 }
