da8e42b62c4f49e638436c658c0ffd58898bd79c
YuryBandarchuk16 <yury.bandarchuk@gmail.com>
2017-05-28 19:19:20 +0300

    Mode2D window updated

---
+++ src/sample/Mode2DWindow.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javax.swing.*;
+
+/**
+ * Created by YuryBandarchuk on 5/28/17.
+ */
+public class Mode2DWindow extends JFrame {
+
+    public Mode2DWindow() {
+        setSize(600, 400);
+        setResizable(false);
+        
+    }
+}
--- src/sample/physics/Field.java
+++ src/sample/physics/Field.java
@@ -8,26 +8,32 @@ import ru.ifmo.ctddev.bandarchuk.expression.*;
  */
 public class Field {
 
-    private TripleExpression expression;
+    private Operand expression;
     private ExpressionParser expressionParser;
 
+    private Operand diffX;
+    private Operand diffY;
+    private Operand diffZ;
 
-    public Field(String expression) {
+
+    public Field(String expression) throws OverflowException, IncorrectExpressionException {
         this.expressionParser = new ExpressionParser();
-        try {
-            this.expression = this.expressionParser.parse(expression);
-        } catch (IncorrectExpressionException e) {
-            System.out.println("Incorrect expression, sorry :(");
-            System.exit(0);
-        } catch (OverflowException e) {
-            System.out.println("overflow happened, sorry :(");
-            System.exit(0);
-        }
+        this.expression = this.expressionParser.parse(expression);
+        this.diffX = this.expression.diff("x");
+        this.diffY = this.expression.diff("y");
+        this.diffZ = this.expression.diff("z");
     }
 
     public double getPhiAt(double x, double y, double z) throws OverflowException, DivisionByZeroException {
         return this.expression.evaluate(x, y, z);
     }
 
+    private double getVectorLength(double xComponent, double yComponent, double zComponent) {
+        return Math.sqrt(xComponent * xComponent + yComponent * yComponent + zComponent * zComponent);
+    }
+
+    public double getEAt(double x, double y, double z) throws OverflowException, DivisionByZeroException {
+        return getVectorLength(diffX.evaluate(x, y, z), diffY.evaluate(x, y, z), diffZ.evaluate(x, y, z));
+    }
 
 }
